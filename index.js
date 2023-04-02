a7e9cddd2fe14809c9cafb1f597f3434003d1237
grigaskate <grigaskate@gmail.com>
2023-04-02 14:05:25 +0300

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const price1 = document.getElementById("price1").innerHTML;
+const price2 = document.getElementById("price2").innerHTML;
+const price3 = document.getElementById("price3").innerHTML;
+const price4 = document.getElementById("price4").innerHTML;
+const sum =
+  parseFloat(price1.replace(",", ".")) +
+  parseFloat(price2.replace(",", ".")) +
+  parseFloat(price3.replace(",", ".")) +
+  parseFloat(price4.replace(",", "."));
+let priceSum = (document.getElementById("priceSum").innerHTML = sum.toFixed(2));
 
+function clickSale20() {
+  priceSum = document.getElementById("priceSum").innerHTML = (
+    sum * 0.8
+  ).toFixed(2);
+}
